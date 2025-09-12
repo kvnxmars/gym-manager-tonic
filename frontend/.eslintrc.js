eefe2acf0971a81b0b45e1e52da9bbd7a6aa707c
kvnxmars <bookingskevinmars@gmail.com>
2025-09-12 11:21:14 +0200

    .eslintrc.js had to be added due to stubborn bugs.

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    // Add custom rules here if needed
+  }
+};
